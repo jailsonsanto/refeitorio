679053c59d36636e76c890ebbc42c9ec2171d04f
jailsonsanto <39930613+jailsonsanto@users.noreply.github.com>
2019-05-17 16:45:20 -0300

    interaçao com banco

---
+++ PHP/entrada.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+	<title>Bem-Vindo ao Cardifpi</title>
+</head>
+<body>
+	        <div class="jumbotron">
+  <h1 class="display-4">Bem Vindo ao Restaurante</h1>
+  <p class="lead">Estamos esperando voce </p>
+  <hr class="my-4">
+  <p>digite sua matricula!</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">continuar</a>
+
+</body>
+</html>
